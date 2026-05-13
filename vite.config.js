1778d5472362900cffa3018aec78a736f784ea17
manish <manish@macbook.m1>
2026-05-14 03:59:36 +0530

    deploy gh

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: "/offline-jam-with-phones/",
   server: { host: true },
   build: { target: 'es2020' }
 });
